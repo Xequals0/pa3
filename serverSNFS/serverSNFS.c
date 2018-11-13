9ed86f1ea65b88c59275ce78533a3b0ae35a6406
Anirudh Tunoori <at813@scarletmail.rutgers.edu>
2018-11-13 18:06:58 -0500

    Initial

---
+++ serverSNFS/serverSNFS.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <pthread.h>
+#include <ctype.h>
+#include <fcntl.h>
+
+int main(int argc, const char* argv[])
+{
+    
+    
+    
+    
+    
+    return 0;
+}
