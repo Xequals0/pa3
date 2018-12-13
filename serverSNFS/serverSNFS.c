636c123a5a1c6a02223306f57df8b258cd2e1bd1
Juan Uribe <juanuribe80@gmail.com>
2018-12-13 18:21:47 -0500

    clean up comments and print statments

--- serverSNFS/serverSNFS.c
+++ serverSNFS/serverSNFS.c
@@ -14,14 +14,6 @@
 #include <dirent.h>
 #include <fuse.h>
 
-
-/* typedef struct File{
-	char *pathname;
-	int fd;
-	int flags;
-	struct File *next;
-} fileNode;
-*/ 
 typedef struct{
 	char ipAddress[INET_ADDRSTRLEN];
 	int command;
@@ -41,42 +33,16 @@ typedef struct{
      11. release
      */
     int fd;
-//	fileNode *database;
-//	int isEmptyDatabase;
 } client_args;
-
-/*typedef struct Node{
-	client_args *client;
-	struct Node *next;
-} node;
-*/
 
 typedef struct sockaddr SA;
 
-//fileNode* searchDataBaseFileName(client_args *client, char *filename);
-//fileNode* searchDataBaseFD(client_args *client, int fd);
-//int removeFileNode(client_args *client, int fd);
 void* selectMethod(void *arg);
-//node* searchListAddress(char *ipAddress);
-//int addClientNode(client_args *client);
-
-
-//node* list = NULL;
-
-/*
-int numReaders;
-int isWriting;
-pthread_mutex_t mutex;
-pthread_cond_t finishedWriting;
-pthread_cond_t finishedReading;
-*/
-
 
 char* mount;
-int main(int argc, char* argv[])//(int argc, const char* argv[])
+int main(int argc, char* argv[])
 {
-	//char* port = argv[1];
-    int port = -1; //This is temporary; We should obtain this from the user.
+    int port = -1;
     int mountarg = 0;
     int i;
     for(i = 1; i < argc; i++)
@@ -95,8 +61,8 @@ int main(int argc, char* argv[])//(int argc, const char* argv[])
         }
     }
 
-    if(mountarg == 0) printf("No mount point specified!\n");
-    if(port == -1) printf("No port number specified!\n");
+  //  if(mountarg == 0) printf("No mount point specified!\n");
+  // if(port == -1) printf("No port number specified!\n");
     if(mountarg == 0 || port == -1) return -1; 
 
     int serverSocket, *clientSocket;
@@ -129,8 +95,6 @@ int main(int argc, char* argv[])//(int argc, const char* argv[])
     }
 
 	client_args *clientinfo = (client_args *)malloc(sizeof(client_args));
-	//clientinfo->database = NULL;
-	//clientinfo->isEmptyDatabase = 1;
     do
     {
 	clientlen = sizeof(struct sockaddr_storage);
@@ -139,13 +103,7 @@ int main(int argc, char* argv[])//(int argc, const char* argv[])
         if (*clientSocket < 0)
         {
             printf("Failed to accept a new connection. Error: %d\n",errno);
-        }/*
-        else
-        {
-            pthread_t tid;
-            pthread_create(&tid, NULL, client_thread_handler, &clientSocket);
-        }*/
-
+        }
 	getnameinfo((SA *)&clientaddr, clientlen, client_hostname, 8192, client_port, 8192, 0);
 
 	clientinfo->fd = *clientSocket;
@@ -159,20 +117,6 @@ int main(int argc, char* argv[])//(int argc, const char* argv[])
 	else
 		perror("error getting the clients IP address\n");
 
-/*	node *newClientNode = (node *) malloc(sizeof(node));
-	newClientNode->client = clientinfo;
-	newClientNode->next = NULL;
-	if(list == NULL)
-		list = newClientNode;
-
-	node *searchList = searchListAddress(clientinfo->ipAddress);
-	if(strcmp(searchList->client->ipAddress, clientinfo->ipAddress) == 0){
-		//already in list
-	}
-	else if(searchList->next == NULL && strcmp(searchList->client->ipAddress, clientinfo->ipAddress) != 0)
-		addClientNode(clientinfo);
-*/
-
 	//get what command is being called, ex: open, close, etc
 	int command = -1;
 	if(recv(*clientSocket, &command, sizeof(command), 0) == -1){
@@ -181,7 +125,6 @@ int main(int argc, char* argv[])//(int argc, const char* argv[])
 	}
 
 	clientinfo->command = ntohl(command);
-	printf("command is: %d\n", clientinfo->command);
 
 	pthread_create(&tid, NULL, selectMethod, (void*)clientinfo);	
 
@@ -190,21 +133,6 @@ int main(int argc, char* argv[])//(int argc, const char* argv[])
     close(serverSocket);
     return 0;
 }
-
-
-void* client_thread_handler(void* clientSock)
-{
-    printf("Hey there!\n");
-    char buffer[256] = "Hey there!\n";
-    //Send Welcome (this is a test)
-    send(*(int*)clientSock, buffer, sizeof(buffer), 0);
-
-    //Fill in the rest of the handler here
-
-    printf("A connection has closed\n");
-    pthread_exit(0);
-}
-
 
 int server_open(client_args *client){
     int pathsize;
@@ -232,9 +160,7 @@ int server_open(client_args *client){
 		printf("Error reading flags from the client\n");
 
 	int flag = ntohl(tmpFlag);
-	printf("flag: %d\n", flag);
-
-    
+	
     int fd = open(filename, flag);
     int fdN = htonl(fd);
 
@@ -305,14 +231,11 @@ int server_read(client_args *client){
     
     if(recv(client->fd, &offsetN, sizeof(int), 0) < 0)
         printf("Error reading offset from the client\n");
-    offset = ntohl(offsetN); 
-	
-    printf("\nsize: %d, offset: %d\n", size, offset);
-
+    offset = ntohl(offsetN);
+    
     char *buf = (char*)malloc(size);
     int res = pread(fh, buf, size, offset);
 
-    printf("res: %d\n", res);
     
     if (res == -1){
         int retVal = htonl(-1);
@@ -376,8 +299,6 @@ int server_write(client_args *client){
     
     int offset = ntohl(offsetN);
 
-    printf("fh: %d\n", fh);
-
     int res = pwrite(fh, buf, size, offset);
     if (res == -1){
         int retVal = htonl(-1);
@@ -411,7 +332,6 @@ int server_flush(client_args *client){
 		perror("Could not receive fd to flush from the client");
 
 	int fd = ntohl(fds);
-	printf("fd going to be flushed is %d\n", fd);
 
     int result = close(dup(fd));
     int res = htonl(result);
@@ -437,7 +357,6 @@ int server_release(client_args *client){
         perror("Could not receive fd to release from the client");
 
     int fd = ntohl(fds);
-    printf("fd going to be released is %d\n", fd);
 
     int result = close(fd);
     int res = htonl(result);
@@ -510,7 +429,6 @@ int server_mkdir(client_args *client){
 }
 
 int server_getattr(client_args *client){
-    puts("ATTR");
 
     int pathsize;
     int recv_file;
@@ -531,8 +449,8 @@ int server_getattr(client_args *client){
     if((recv_path = recv(client->fd, pathBuffer, pathLen, 0)) == -1)
         perror("Error reading path from the client\n");
     
-    strcat(path, pathBuffer);   
-    printf("!---%s\n", path); 
+    strcat(path, pathBuffer);
+    
     struct stat *stbuf = malloc(sizeof(struct stat));
     int result = lstat(path, stbuf);
     
@@ -553,13 +471,12 @@ int server_getattr(client_args *client){
         if(send(client->fd, stbuf, sizeof(struct stat), 0) == -1)
             perror("Could not send the  stat struct to the client\n");
     }
-    printf("!!--%d\n",result);
+    
     return result;
 
 }
 
 int server_readdir(client_args *client){
-    puts("READDIR");
     DIR *dp;
     struct dirent *de;
     int fd;
@@ -571,7 +488,6 @@ int server_readdir(client_args *client){
     int fh = ntohl(fd);
 
     dp = fdopendir(fh);
-    //send error
     
     int z;
     while ((de = readdir(dp)) != NULL) {
@@ -697,7 +613,6 @@ int server_opendir(client_args *client){
 }
 
 int server_releasedir(client_args *client){
-    puts("releasedir");
     int fds = -1;
     DIR *dp;
 
@@ -705,11 +620,10 @@ int server_releasedir(client_args *client){
         perror("Could not receive fd to release from the client");
 
     int fd = ntohl(fds);
-    printf("fd going to be released is %d\n", fd);
 
     dp = fdopendir(fd);
     int result = closedir(dp);
-    printf("--closeres: %d\n", result);
+    
     int res = htonl(result);
 
     //send result
@@ -727,8 +641,6 @@ int server_releasedir(client_args *client){
 }
 
 int server_create(client_args *client){
-    puts("create");
-
     int pathsize;
     int recv_file;
 
@@ -748,8 +660,7 @@ int server_create(client_args *client){
     if((recv_path = recv(client->fd, pathBuffer, pathLen, 0)) == -1)
         perror("Error reading path from the client\n");
     
-    strcat(path, pathBuffer);  
-    printf("!---%s\n", path);
+    strcat(path, pathBuffer);
 
     //recv mode
     mode_t mode;
@@ -759,7 +670,6 @@ int server_create(client_args *client){
         printf("Error reading mode from the client\n");
     
     mode = ntohl(modeN);
-    printf("mode::: %d\n", mode);
         
    int fd = open(path, O_CREAT | O_EXCL | O_WRONLY, mode);
    int send_fd = htonl(fd);
@@ -783,55 +693,40 @@ void* selectMethod(void *arg){
 	int command = args->command;
 	
 	if(command == 0){
-		printf("calling server_open\n");
 		server_open(args);
 	}
 	else if(command == 1){ //read
-        printf("calling server_read");
         server_read(args);
     }
 	else if(command == 2){ //write
-        printf("calling server_write");
         server_write(args);
     }
 	else if(command == 3){
-		printf("calling server_flush");
 		server_flush(args);
 	}
     else if(command == 4){ //create
-        puts("calling create");
         server_create(args);
     }
     else if(command == 5){ //truncate
-        puts("calling truncate");
         server_truncate(args);
     }
     else if(command == 6){ //getattr
-        //printf("calling server_getattr");
         server_getattr(args);
     }
     else if(command == 7){ //opendir
-        puts("calling opendir");
         server_opendir(args);
     }
     else if(command == 8){ //readdir
-        printf("calling server_readdir");
         server_readdir(args);
     }
     else if(command == 9){ //releasedir
-        printf("calling server_releasedir");
         server_releasedir(args);
     }
     else if(command == 10){ //mkdir
-        puts("calling server_mkdir");
         server_mkdir(args);
     }
     else if(command == 11){ //mkdir
-        puts("calling server_release");
         server_release(args);
-    }
-    else{
-        printf("No valid method was called\n");	
     }
     
     return NULL;	
